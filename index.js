914acedb37a651306ecbe750555087d4e33c2e30
g0blin <742005790@qq.com>
2022-09-17 01:36:46 +0800

    新增对 csdn 的支持

--- index.js
+++ index.js
@@ -1,94 +1,122 @@
 // ==UserScript==
 // @name                净化
-// @version             1.0.2
-// @description         用于剔除页面上的广告（百度百科，csdn等），删除 csdn 登录提示，自动展开等。
+// @version             1.1.0
+// @description         百度、CSDN页面增强工具
 // @author              g0blin
 // @match               *://baike.baidu.com/item/*
 // @match               *://zhidao.baidu.com/*
 // @match               *://jingyan.baidu.com/article/*
+// @match               *://*.csdn.net/*
+// @match               *://blog.csdn.net/*
 // @icon                https://ossweb-img.qq.com/images/lol/img/spell/SummonerBoost.png
-// @grant               none
+// @require             https://www.unpkg.com/jquery.cookie@1.4.1/jquery.cookie.js
 // @license             WTFPL
 // ==/UserScript==
 
 (function () {
     'use strict';
 
-    const _VERSION_ = '1.0.2'
+    const VERSION = '1.1.0';
+    const BAIDU_URL_REG = RegExp('baidu.com');
+    const CSDN_URL_REG = RegExp('csdn.net');
+    const CURRENT_URL = window.location.href;
 
     /**
-     * 老方法，获取待删除节点的父节点，然后用父节点删除目标节点 已废除
-     * @param {待删除元素的class名称}
+     * 循环删除
+     * @param classNameArray 待删除的元素类名的数组
      */
-    function removeElementByHimFather(className) {
-        //获取待删除元素
-        var removeEle = document.querySelector('.' + className);
-        if (removeEle !== null) {
-            // 获取目标节点的父节点
-            var parent = removeEle.parentElement;
-            // 用目标节点的父节点删除目标节点
-            parent.removeChild(removeEle);
+    function loopRemoveByClassName(classNameArray) {
+        for (const classNameArrayElement of classNameArray) {
+            $('.' + classNameArrayElement).remove();
+        }
+    }
+
+    function loopRemoveById(idArray) {
+        for (const idArrayElement of idArray) {
+            $('#' + idArrayElement).remove();
         }
     }
 
     /**
-     * 进一步封装删除的方法
-     * @param {待删除的广告元素class名称数组} classNameArray
-     */
-    function removeByLoopArray(classNameArray) {
-        for (var className of classNameArray) {
-            // 移除元素
-            removeElementByHimFather(className);
-        }
-    }
-
-    /**
-     * 按周期来调用
-     * @param {待处理的元素className数组} classNameArray
-     * @param {回调函数} callbackFunc
-     * @param {定时时间} time
-     */
-    var safeWaitFunc = function (classNameArray, callbackFunc, time) {
-        time = time || 50;
-        var id = setInterval(function () {
-            clearInterval(id);
-            callbackFunc(classNameArray);
-        }, time)
-    };
-
-    function createBox() {
-        var clearBox = document.createElement('div');
-        var TEXT = document.createTextNode(`[净化]已启用 v${_VERSION_} by g0blin`);
-
-        clearBox.style['background-color'] = '#dfe4ea';
-        clearBox.style['color'] = '#747d8c';
-        clearBox.style['font-size'] = '8px';
-        clearBox.style['display'] = 'inline-block';
-        clearBox.style['padding'] = '5px';
-        clearBox.style['border-radius'] = '8px';
-        clearBox.style['position'] = 'fixed';
-        clearBox.style['bottom'] = '200px';
-        clearBox.style['left'] = '0px';
-        clearBox.style['user-select'] = 'none';
-        clearBox.appendChild(TEXT);
-
-        document.body.appendChild(clearBox);
-    }
-
-    /**
-     * 打印脚本信息
+     * 控制台打印脚本信息
      */
     function printInfo() {
-        console.log(`%c[净化]已启用 v${_VERSION_} by g0blin`, "color:darkorange;background:#57606f;padding:5px;border-radius:8px;font-family:SimSun;font-size:8px");
-        createBox();
+        console.log(`%c[净化]已开启 当前版本：${VERSION} by g0blin`, "color:darkorange;background:#57606f;padding:5px;border-radius:8px;font-family:SimSun;font-size:8px");
     }
 
-    //************************************************************************************* */
-    var classNameArray = ['secondsknow-large-container', 'lemmaWgt-promotion-vbaike', 'lemmaWgt-promotion-slide', 'topA', 'appdownload', 'share-list', 'tashuo-right', 'tashuo-bottom', 'poster-middle', 'feeds-video-box', 'feeds-video-one-view', 'wgt-cms-banner', 'fresh-share-exp-e', 'J-secondsknow-large-container', 'unionAd', 'after-content', 'right-ad', 'task-panel', 'task-list-button', 'main-menu'];
-    // timerToRemove(classNameArray);
-    safeWaitFunc(classNameArray, removeByLoopArray, 500);
+    if (BAIDU_URL_REG.test(CURRENT_URL)) {
+        // 待移除的元素类名
+        const classNameArray = ['secondsknow-large-container', 'lemmaWgt-promotion-vbaike', 'lemmaWgt-promotion-slide', 'topA', 'appdownload', 'share-list', 'tashuo-right', 'tashuo-bottom', 'poster-middle', 'feeds-video-box', 'feeds-video-one-view', 'wgt-cms-banner', 'fresh-share-exp-e', 'J-secondsknow-large-container', 'unionAd', 'after-content', 'right-ad', 'task-panel', 'task-list-button', 'main-menu'];
 
-    //打印信息
+        loopRemoveByClassName(classNameArray);
+    }
+
+    if (CSDN_URL_REG.test(CURRENT_URL)) {
+        // 待移除的元素类名
+        const classNameArray = ['box-shadow', 'programmer1Box', 'programmer2Box', 'programmer3Box', 'programmer4Box', 'template-box', 'template-box'];
+        // 待移除的元素 id
+        const idArray = ['recommendAdBox', 'asideNewNps', 'recommendNps'];
+
+        loopRemoveByClassName(classNameArray);
+        loopRemoveById(idArray);
+
+        $("iframe").remove();
+
+        // 禁止弹出登录窗口
+        setInterval(() => {
+            $('.passport-login-container').remove();
+        }, 500);
+
+        $.cookie('unlogin_scroll_step', 0, {
+            domain: '.csdn.net',
+            path: '/'
+        });
+
+        // 解除代码窗禁用选择
+        $("code").css("user-select", "auto");
+        $("#content_views").css("user-select", "auto");
+        $("pre").css("user-select", "auto");
+
+        // 免登录复制
+        $(".hljs-button").removeClass("signin");
+        $(".hljs-button").addClass("{2}");
+        $(".hljs-button").attr("data-title", "[净化]已解除复制限制-点击复制");
+        $(".hljs-button").attr("onclick", "hljs.copyCode(event)");
+        $("code").attr("onclick", "mdcp.copyCode(event)");
+
+        // 默认隐藏文章作者的更多信息
+        $('.data-info,.aside-box-footer,.profile-intro-name-boxOpration,.item-rank').wrapAll('<div id="data-info-box" style="display: none"></div>');
+        $('#asideProfile').append('<div id="data-info-mask" style="height: 40px;text-align: center;line-height: 47px;cursor: pointer;color:tomato" onclick="showInfo()">点击此处查看作者信息</div>');
+
+        // 显示作者信息
+        $('#data-info-mask').click(function () {
+            $('#data-info-box').css('display', 'block');
+            $('#data-info-mask').css('display', 'none');
+        });
+
+        // 解除关注博主后阅读全文
+        if ($('.hide-article-box').length > 0) {
+            $('.hide-article-box').remove();
+            $('article #article_content').css('height', '');
+        }
+
+        // 代码自动展开
+        $('.hide-preCode-bt').click();
+
+        // 未登录移除顶部多余内容
+        $('.toolbar-container-right').remove();
+        $('.toolbar-container-middle').remove();
+        $('.toolbar-container-left .toolbar-menus').html('<li style="color: forestgreen;user-select: none">[净化]正在优化 CSDN 的浏览体验。</li><li><a style="color: cornflowerblue" href="https://github.com/ultrazg/tm-cleanup/issues" target="_blank">[反馈]</a></li>');
+        $('.toolbar-menus li').css('margin-right', '10px');
+        $('.toolbar-menus li').hover(function () {
+            $(this).css('background', '#fff');
+        });
+
+        // 优化底部点赞条
+        $('#toolBarBox .left-toolbox').remove();
+
+    }
+
     printInfo();
 
 })();
