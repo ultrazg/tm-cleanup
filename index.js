21f507e64645d8382be739d8613da7f1881041c3
clearw1ne <742005790@qq.com>
2022-12-17 03:46:08 +0800

    feat: 新增去除 CSDN 顶部广告栏

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 // ==UserScript==
-// @name                净化
-// @version             1.1.0
-// @description         百度、CSDN页面增强工具
+// @name                净化-开发版
+// @version             1.2.0
+// @description         页面增强工具。目前支持CSDN、百度、简书、掘金。
 // @author              g0blin
-// @match               *://baike.baidu.com/item/*
+// @namespace           https://github.com/ultrazg/tm-cleanup
+// @match               *://baike.baidu.com/*
 // @match               *://zhidao.baidu.com/*
-// @match               *://jingyan.baidu.com/article/*
+// @match               *://jingyan.baidu.com/*
 // @match               *://*.csdn.net/*
 // @match               *://blog.csdn.net/*
+// @match               *://*.jianshu.com/*
 // @icon                https://ossweb-img.qq.com/images/lol/img/spell/SummonerBoost.png
+// @require             https://www.unpkg.com/jquery@1.12.4/dist/jquery.min.js
 // @require             https://lf26-cdn-tos.bytecdntp.com/cdn/expire-1-M/jquery-cookie/1.4.1/jquery.cookie.min.js
 // @license             WTFPL
+// @grant               unsafeWindow
 // ==/UserScript==
 
 (function () {
     'use strict';
 
-    const VERSION = '1.1.0';
+    const VERSION = '1.2.0';
     const BAIDU_URL_REG = RegExp('baidu.com');
+    const BAIDU_BAIKE_URL_REG = RegExp('baike.baidu.com');
+    const BAIDU_ZHIDAO_URL_REG = RegExp('zhidao.baidu.com/question');
+    const BAIDU_JINGYAN_URL_REG = RegExp('jingyan.baidu.com');
     const CSDN_URL_REG = RegExp('csdn.net');
     const CURRENT_URL = window.location.href;
 
@@ -44,16 +51,42 @@
         console.log(`%c[净化]已开启 当前版本：${VERSION} by g0blin`, "color:darkorange;background:#57606f;padding:5px;border-radius:8px;font-family:SimSun;font-size:8px");
     }
 
+    /** 百度 */
     if (BAIDU_URL_REG.test(CURRENT_URL)) {
-        // 待移除的元素类名
-        const classNameArray = ['secondsknow-large-container', 'lemmaWgt-promotion-vbaike', 'lemmaWgt-promotion-slide', 'topA', 'appdownload', 'share-list', 'tashuo-right', 'tashuo-bottom', 'poster-middle', 'feeds-video-box', 'feeds-video-one-view', 'wgt-cms-banner', 'fresh-share-exp-e', 'J-secondsknow-large-container', 'unionAd', 'after-content', 'right-ad', 'task-panel', 'task-list-button', 'main-menu'];
+        /** 百度百科 */
+        if (BAIDU_BAIKE_URL_REG.test(CURRENT_URL)) {
+            const classNameArr = ['header', 'navbar-wrapper', 'polysemant-list', 'btn-list', 'right-ad', 'after-content', 'related-video-container'];
+            const idArr = ['J-second-wrapper', 'tashuo_right', 'side_box_unionAd', 'side-share', 'tashuo_bottom'];
 
-        loopRemoveByClassName(classNameArray);
+            setInterval(() => {
+                loopRemoveByClassName(classNameArr);
+                loopRemoveById(idArr);
+            }, 500);
+
+            const wikiCommonHeadTabBarDom = `
+                <a style="color: forestgreen;user-select: none;font-weight: normal;text-decoration: none">[净化]正在优化 百度百科 的浏览体验。</a>
+                <a style="color: cornflowerblue;font-weight: normal;text-decoration: none" href="https://github.com/ultrazg/tm-cleanup/issues" target="_blank">[反馈]</a>
+            `;
+
+            $('.wiki-common-headTabBar').html(wikiCommonHeadTabBarDom);
+        }
+
+        /** 百度知道 */
+        // if (BAIDU_ZHIDAO_URL_REG.test(CURRENT_URL)) {
+        //     const classNameArr = ['grid', 'task-list-button', 'jump-goto-star', 'aria-div', 'question-number-text-chain', 'wgt-bottom-union'];
+        //     const idArr = ['j-nav-menu-container', 'qb-side'];
+        //
+        //     setInterval(() => {
+        //         loopRemoveByClassName(classNameArr);
+        //         loopRemoveById(idArr);
+        //     }, 500);
+        // }
     }
 
+    /** CSDN */
     if (CSDN_URL_REG.test(CURRENT_URL)) {
         // 待移除的元素类名
-        const classNameArray = ['box-shadow', 'programmer1Box', 'programmer2Box', 'programmer3Box', 'programmer4Box', 'template-box', 'template-box'];
+        const classNameArray = ['box-shadow', 'programmer1Box', 'programmer2Box', 'programmer3Box', 'programmer4Box', 'template-box', 'template-box', 'toolbar-advert'];
         // 待移除的元素 id
         const idArray = ['recommendAdBox', 'asideNewNps', 'recommendNps'];
 
@@ -104,9 +137,13 @@
         $('.hide-preCode-bt').click();
 
         // 未登录移除顶部多余内容
+        const toolBarBoxDom = `
+        <li style="color: forestgreen;user-select: none">[净化]正在优化 CSDN 的浏览体验。</li><li><a style="color: cornflowerblue" href="https://github.com/ultrazg/tm-cleanup/issues" target="_blank">[反馈]</a></li>
+        `;
+
         $('.toolbar-container-right').remove();
         $('.toolbar-container-middle').remove();
-        $('.toolbar-container-left .toolbar-menus').html('<li style="color: forestgreen;user-select: none">[净化]正在优化 CSDN 的浏览体验。</li><li><a style="color: cornflowerblue" href="https://github.com/ultrazg/tm-cleanup/issues" target="_blank">[反馈]</a></li>');
+        $('.toolbar-container-left .toolbar-menus').html(toolBarBoxDom);
         $('.toolbar-menus li').css('margin-right', '10px');
         $('.toolbar-menus li').hover(function () {
             $(this).css('background', '#fff');
